1d45bb5b38710f396f6bf3dd7a7c3cc6e025ecda
yeetree <64941543+yeetree@users.noreply.github.com>
2022-03-22 18:57:00 -0500

    arch_js16 v0.2 compiler
    doesn't include a way to load/store physical memory, and functionality to call other programs stored (in RAM or Physical Storage)

--- assembler/asm.js
+++ assembler/asm.js
@@ -22,8 +22,35 @@ class asm
         ["MOVER", 2, 16],
         ["SWAPR", 2, 17],
         //Video
-        ["UPDATE", 0, 18]
-        
+        ["UPDATE", 0, 18],
+        //Input
+        ['IN', 1, 19],
+        ['NUMIN', 1, 20],
+        ['RGIN', 2, 21],
+        //Control
+        ['IFEQ', 3, 22],
+        ['NOTEQ', 3, 23],
+        ['IFGT', 3, 24],
+        ['NOTGT', 3, 25],
+        ['IFLT', 3, 26],
+        ['NOTLT', 3, 27],
+        ['WHILE', 3, 28],
+        ['LOOP', 3, 29],
+
+        ['VIFEQ', 3, 30],
+        ['VNOTEQ', 3, 31],
+        ['VIFGT', 3, 32],
+        ['VNOTGT', 3, 33],
+        ['VIFLT', 3, 34],
+        ['VNOTLT', 3, 35],
+        ['VWHILE', 3, 36],
+        ['VLOOP', 3, 37],
+
+        ['FUNC', 1, 38],
+        ['VFUNC', 1, 39],
+
+        ['CALL', 1 , 40],
+        ['VCALL', 1 , 41]
     ]
     prg = [];
     lines = [];
@@ -36,75 +63,232 @@ class asm
         
         for(let i = 0; i < this.lines.length; i++)
         {
-            this.inst = this.lines[i].split(',');
-            switch(this.inst[0].toUpperCase())
-            {
-                //Standard RAM
-                case "SET":
-                    this.prg.push(0, this.inst[i + 1], this.inst[i + 2]);
-                    console.log(this.inst)
-                    break;
-                case "MOVE":
-                    this.prg.push(1, this.inst[i + 1], this.inst[i + 2]);
-                    break;
-                case "SWAP":
-                    this.prg.push(2, this.inst[i + 1], this.inst[i + 2]);
-                    break;
-                case "ADD":
-                    this.prg.push(3, this.inst[i + 1], this.inst[i + 2]);
-                    break;
-                case "SUB":
-                    this.prg.push(4, this.inst[i + 1], this.inst[i + 2]);
-                    break;         
-                case "MUL":
-                    this.prg.push(5, this.inst[i + 1], this.inst[i + 2]);
-                    break;
-                case "DIV":
-                    this.prg.push(6, this.inst[i + 1], this.inst[i + 2]);
-                    break;
-                case "MOVEV":
-                    this.prg.push(7, this.inst[i + 1], this.inst[i + 2]);
-                    console.log(this.inst)
-                    break;
-                case "SWAPV":
-                    this.prg.push(8, this.inst[i + 1], this.inst[i + 2]);
-                    break;
-
-
-                //Video RAM
-                case "VSET":
-                    this.prg.push(9, this.inst[i + 1], this.inst[i + 2]);
-                    break;
-                case "VMOVE":
-                    this.prg.push(10, this.inst[i + 1], this.inst[i + 2]);
-                    break;
-                case "VSWAP":
-                    this.prg.push(11, this.inst[i + 1], this.inst[i + 2]);
-                    break;
-                case "VADD":
-                    this.prg.push(12, this.inst[i + 1], this.inst[i + 2]);
-                    break;
-                case "VSUB":
-                    this.prg.push(13, this.inst[i + 1], this.inst[i + 2]);
-                    break;         
-                case "VMUL":
-                    this.prg.push(14, this.inst[i + 1], this.inst[i + 2]);
-                    break;
-                case "VDIV":
-                    this.prg.push(15, this.inst[i + 1], this.inst[i + 2]);
-                    break;
-                case "MOVER":
-                    this.prg.push(16, this.inst[i + 1], this.inst[i + 2]);
-                    break;
-                case "SWAPR":
-                    this.prg.push(17, this.inst[i + 1], this.inst[i + 2]);
-                    break;
-                //Video
-                case "UPDATE":
-                    this.prg.push(18);
-                    break;
-            }
+            this.inst = this.lines[i].toUpperCase().split(',');
+            this.prg.push(this.getInst(this.inst));
         }
         return this.prg;
+    }
+    getInst(inst)
+    {
+        let prgSeg = [];
+        switch(inst[0])
+        {
+            //Standard RAM
+            case "SET":
+                prgSeg.push(0);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "MOVE":
+                prgSeg.push(1);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "SWAP":
+                prgSeg.push(2);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "ADD":
+                prgSeg.push(3);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "SUB":
+                prgSeg.push(4);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "MUL":
+                prgSeg.push(5);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "DIV":
+                prgSeg.push(6);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "MOVEV":
+                prgSeg.push(7);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "SWAPV":
+                prgSeg.push(8);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            //Video RAM
+            case "VSET":
+                prgSeg.push(9);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "VMOVE":
+                prgSeg.push(10);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "VSWAP":
+                prgSeg.push(11);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "VADD":
+                prgSeg.push(12);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "VSUB":
+                prgSeg.push(13);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "VMUL":
+                prgSeg.push(14);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "VDIV":
+                prgSeg.push(15);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "MOVER":
+                prgSeg.push(16);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "SWAPR":
+                prgSeg.push(17);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            //Video
+            case "UPDATE":
+                prgSeg.push(18);
+                break;
+
+            //Input
+            case "IN":
+                prgSeg.push(19);
+                prgSeg.push(inst[1]);
+                break;
+            case "NUMIN":
+                prgSeg.push(inst[1]);
+                prgSeg.push(20);
+                break;
+            case "RGIN":
+                prgSeg.push(21);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+
+            //Control
+            case "IFEQ":
+                prgSeg.push(22);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                prgSeg.push(inst[3]);
+                break;
+            case "NOTEQ":
+                prgSeg.push(23);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                prgSeg.push(inst[3]);
+                break;
+            case "IFGT":
+                prgSeg.push(24);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "NOTGT":
+                prgSeg.push(25);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                prgSeg.push(inst[3]);
+                break;
+            case "IFLT":
+                prgSeg.push(26);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                prgSeg.push(inst[3]);
+                break;
+            case "NOTLT":
+                prgSeg.push(27);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "WHILE":
+                prgSeg.push(28);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                prgSeg.push(inst[3]);
+                break;
+            case "LOOP":
+                prgSeg.push(29);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            
+            case "VIFEQ":
+                prgSeg.push(30);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                prgSeg.push(inst[3]);
+                break;
+            case "VNOTEQ":
+                prgSeg.push(31);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                prgSeg.push(inst[3]);
+                break;
+            case "VIFGT":
+                prgSeg.push(32);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "VNOTGT":
+                prgSeg.push(33);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                prgSeg.push(inst[3]);
+                break;
+            case "VIFLT":
+                prgSeg.push(34);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                prgSeg.push(inst[3]);
+                break;
+            case "VNOTLT":
+                prgSeg.push(35);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "VWHILE":
+                prgSeg.push(36);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                prgSeg.push(inst[3]);
+                break;
+            case "VLOOP":
+                prgSeg.push(37);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+
+            case "FUNC":
+                prgSeg.push(38);
+                prgSeg.push(inst[1]);
+                prgSeg.push(inst[2]);
+                break;
+            case "CALL":
+                prgSeg.push(39);
+                prgSeg.push(inst[1]);
+                break;
+        }
+        
+        return prgSeg;
     }
 }
